8cf73766d06363aee4e9d12046013f1aab54de9b
Cole55512 <55994356+Cole55512@users.noreply.github.com>
2020-03-27 14:45:02 -0500

    Update Handler.java

--- Handler.java
+++ Handler.java
@@ -1,10 +1,10 @@
 import java.awt.Graphics;
 import java.util.LinkedList;
 
-public class Handler {
-	LinkedList<GameObject> objects = new LinkedList<GameObject>();
+public class Handler {	
+	LinkedList<GameObject> objects = new LinkedList<GameObject>();	// stores all game objects
 	
-	public void tick() {
+	public void tick() {	// runs tick method of every game object
 		for(int i = 0; i < objects.size(); i++)
 		{
 			GameObject tempObj = objects.get(i);
@@ -12,7 +12,7 @@ public class Handler {
 		}
 	}
 	
-	public void render(Graphics g) {
+	public void render(Graphics g) {	// runs render method of every game object
 		for(int i = 0; i < objects.size(); i++)
 		{
 			GameObject tempObj = objects.get(i);
@@ -20,15 +20,15 @@ public class Handler {
 		}
 	}
 	
-	public void addObject(GameObject object) {
+	public void addObject(GameObject object) {	// adds gameobject to list
 		this.objects.add(object);
 	}
 	
-	public void removeObject(GameObject object) {
+	public void removeObject(GameObject object) {	// removes game object from list
 		this.objects.remove(object);
 	}
 	
-	public void clear() {
+	public void clear() {	// clears the list
 		for(int i = objects.size() - 1; i >= 0; i--)
 		{
 			removeObject(objects.get(i));
