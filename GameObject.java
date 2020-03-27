18b366916bf785b76ef1c389a250b92162c2547c
Cole55512 <55994356+Cole55512@users.noreply.github.com>
2020-03-27 14:41:33 -0500

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -3,40 +3,40 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public abstract class GameObject {
-	protected int x;
-	protected int y;
-	protected ID id;
+	protected int x;	// x position of game object
+	protected int y;	// y position of game object
+	protected ID id;	// id of game object
 	
-	public GameObject(int x, int y, ID id) {
+	public GameObject(int x, int y, ID id) {	//Constructor
 		this.x = x;
 		this.y = y;
 		this.id = id;
 	}
 	
-	public abstract void render(Graphics g);
+	public abstract void render(Graphics g);	// Abstract render method for on screen image
 	public abstract Rectangle getBounds();	//Collision rectangle
 	
-	public int getX() {
+	public int getX() {	// returns x position
 		return x;
 	}
 	
-	public void setX(int x) {
+	public void setX(int x) {	// sets x position
 		this.x = x;
 	}
 	
-	public int getY() {
+	public int getY() {	// returns y position
 		return y;
 	}
 	
-	public void setY(int y) {
+	public void setY(int y) {	// sets y position
 		this.y = y;
 	}
 	
-	public ID getID() {
+	public ID getID() {	// returns game object ID
 		return id;
 	}
 	
-	public void setID(ID id) {
+	public void setID(ID id) {	// sets game object ID
 		this.id = id;
 	}
 }
